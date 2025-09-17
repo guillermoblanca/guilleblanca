f2e6849e2787ccb8963df5909b2fc2fc615362f4
Guille <guillermo_848@hotmail.com>
2025-09-17 16:26:54 +0200

    refactorized

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
