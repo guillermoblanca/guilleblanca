637f2dc492bd3855e37b1caefb6088d2992df57c
guillermoblanca <merlockes@gmail.com>
2026-04-27 21:17:58 +0200

    Delete game directory

--- game/tiles.js
+++
@@ -1,9 +0,0 @@
-// tiles.js
-export const TILE_SIZE = 32; // tamaño de cada tile en px
-
-export let tiles = [
-  {x: 0, y: 400, img: 'assets/tiles/tile1.png'},
-  {x: 32, y: 400, img: 'assets/tiles/tile1.png'},
-  {x: 64, y: 400, img: 'assets/tiles/tile2.png'},
-  // etc...
-];
